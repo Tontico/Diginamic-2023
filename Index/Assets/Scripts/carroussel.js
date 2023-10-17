084e86a1f64ce2c926cd84e1a2ed2aa3d53ee6de
Tontico <144102137+Tontico@users.noreply.github.com>
2023-10-17 14:13:50 +0200

    Add files via upload

---
+++ Index/Assets/Scripts/carroussel.js
@@ -0,0 +1,21 @@
+const slides = document.querySelectorAll('.carousel-slide');
+let currentSlideIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  showSlide(currentSlideIndex);
+}
+
+setInterval(nextSlide, 4000); 
+
+showSlide(currentSlideIndex);
---
+++ Index/Assets/Scripts/title.js
@@ -0,0 +1,20 @@
+const nameElement = document.querySelector('.title ');
+
+nameElement.addEventListener('mouseover', function() {
+  nameElement.style.color = 'rgb(34, 181, 226)';
+});
+
+nameElement.addEventListener('mouseout', function() {
+  nameElement.style.color = 'white';
+});
+
+
+const nameElement1 = document.querySelector('.right-side h3');
+
+nameElement1.addEventListener('mouseover', function() {
+  nameElement1.style.color = 'rgb(34, 181, 226)';
+});
+
+nameElement1.addEventListener('mouseout', function() {
+  nameElement1.style.color = 'white';
+});
